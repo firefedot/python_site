7d4b93badd716be4543e4fcaed2af975922f1f01
a.fedotov <a.fedotov@haulmont.com>
2020-11-15 22:17:22 +0400

    add lines

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from horoscope import generate_prophecies
 from datetime import datetime as dt
 
 
+bootstrap = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">'
+
+
 def generate_page(head, body):
     page = f'<html>{head}{body}</html>'
     return page
@@ -12,6 +15,7 @@ def generate_page(head, body):
 def generate_head(title):
     head = f'''
     <head>
+    {bootstrap}
     <meta charset="UTF-8">
         <title>{title}</title>
     </head>
@@ -24,14 +28,14 @@ def generate_body(header, paragraphs):
     for p in paragraphs:
         body += f"<p>{p}</p>"
 
-    return f"<body>{ body }</body>"
+    return f"<body>{ body }<a href='about.html'>About</a></body>"
 
 
 # body = generate_body(header='Гороскоп на 15.11.2020', paragraphs=generate_prophecies())
 
 
 def save_page(title, header, paragraphs, output="index.html"):
-    with open("index.html", "w") as nf:
+    with open(output, "w") as nf:
         today = dt.now().date()
         page = generate_page(head=generate_head(title), body=generate_body(header=header, paragraphs=paragraphs))
         print(page, file=nf)
@@ -39,3 +43,4 @@ def save_page(title, header, paragraphs, output="index.html"):
 
 today = dt.now().date()
 save_page(title="Гороскоп на сегодня", header=f"Что день {today} говорит", paragraphs=generate_prophecies())
+save_page(title="Гороскоп на сегодня", header=f"page about", paragraphs='any text', output="about.html")
